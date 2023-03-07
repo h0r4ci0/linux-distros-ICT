8d1e9bc65e32ba49f4156da550bb7db5ed7ffbc7
h0r4ci0 <horamalean@protonmail.com>
2023-03-07 23:48:19 +0100

    prettier code with vim

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Linux Distros
+
 ### Task: make a web page - 4º ESO B
 
 #### Here you can view the web page: [h0r4ci0.github.io/linux-distros-ict/](https://h0r4ci0.github.io/linux-distros-ICT/)
--- js/script.js
+++ js/script.js
@@ -20,29 +20,32 @@ redirectButton.addEventListener("click", function () {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date().getTime() + (105 * 24 * 60 * 60 * 1000) + (2 * 60 * 60 * 1000) + (6 * 60 * 1000);
+var countDownDate =
+  new Date().getTime() +
+  105 * 24 * 60 * 60 * 1000 +
+  2 * 60 * 60 * 1000 +
+  6 * 60 * 1000;
 
 // Update the countdown every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
-  
+
   // Calculate the distance between now and the countdown date
   var distance = countDownDate - now;
-  
+
   // Calculate days, hours, minutes and seconds left
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
+
   // Display the countdown in the HTML
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
-  
+
   // If the countdown is finished, show an alert
   if (distance < 0) {
     clearInterval(x);
