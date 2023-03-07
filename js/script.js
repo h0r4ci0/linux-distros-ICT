e1aa060cfd9a9120ed11b75a2a4d21b50f0ee29e
h0r4ci0 <horamalean@protonmail.com>
2023-03-07 20:16:58 +0100

    js buttons

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const loginButton = document.getElementById("login");
+loginButton.addEventListener("click", function () {
+  window.open("https://youtu.be/dQw4w9WgXcQ", "_blank");
+  alert("🎶 Never gonna give you up\nNever gonna let you down...🎶 ");
+});
